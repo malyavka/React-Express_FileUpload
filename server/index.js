c0521f01e9a92fd81fa4598545b27c43729e507c
Maria Obaturova <m.obaturova@gmail.com>
2020-04-03 22:07:00 -0400

    feat: initialised  an express app

---
+++ server/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const morgan = require('morgan');
+const fileUpload = require('express-fileupload');
+const path = require('path');
+
+const db = require('./db');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+module.exports = app;
+
+const createApp = () => {
+    //logging middleware
+    app.use(morgan('dev'));
+
+    //body parser
+    app.use(express.json());
+    app.use(express.urlencoded({
+        extended: true
+    }));
+
+    //statuc file-serving middleware
+    app.use(express.static(path.join(__dirname, '..', 'public')));
+
+    // upload files
+    app.use(fileUpload());
+
+    // error handling endware
+    app.use((err, req, res, next) => {
+        console.error(err);
+        console.error(err.stack);
+        res.status(err.status || 500).send(err.message || 'Internal server error.')
+    })
+
+};
+
+const startListening = () => {
+    // start listening (and create a 'server' object representing our server)
+    app.listen(PORT, () =>
+        console.log(`Collecting investors info on ${PORT}`)
+    )
+
+};
+
+const syncDb = () => db.sync();
+
+async function bootApp() {
+    await syncDb();
+    await createApp();
+    await startListening()
+}
+
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+    bootApp()
+} else {
+    createApp()
+}
